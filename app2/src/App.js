ae7a4fc54d4df76bfabb9c14d4388315c8d6a801
santiaago <santiago.arias@red-gate.com>
2017-08-04 14:48:12 +0100

    skeleton of kata2

---
+++ app2/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Kata 2- Add and remove objects</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ kata2.md
@@ -0,0 +1,13 @@
+# React App Kata 1
+
+Code for **Kata 2** is available in the `app2` folder.
+
+## Learning aims
+
+The idea here is understand the concept of a `state` in a React component. How to work with it and modify it.
+
+## Task
+
+Write the JavaScript/React code to:
+
+1. Todo
