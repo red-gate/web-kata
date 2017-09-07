9ab877112117c59e045d5b38616a245f635e5f95
santiaago <santiago.arias@red-gate.com>
2017-09-07 08:16:33 +0100

    app5 web app start up

---
+++ app5/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return <div className="App">
+      <div className="App-header">
+        <h2>Kata 5 - Interaction with backend server through REST API calls</h2>
+      </div>
+    </div>
+  }
+}
+
+export default App
