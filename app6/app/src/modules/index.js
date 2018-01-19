efcfaadfa78e45e173b8dc62fc037d0b05ab1def
santiaago <santiago.ariassar@gmail.com>
2018-01-19 17:43:56 +0000

    add versions reducer

--- app6/app/src/modules/index.js
+++ app6/app/src/modules/index.js
@@ -2,7 +2,9 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 
+import versions from './versions'
 
 export default combineReducers({
   routing: routerReducer,
+  versions
 })
