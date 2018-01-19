e63b98e7e316cdb0f8cff1cd5cb470f84e450ec8
santiaago <santiago.ariassar@gmail.com>
2018-01-19 17:43:49 +0000

    remove products reducer

--- app6/app/src/modules/index.js
+++ app6/app/src/modules/index.js
@@ -2,9 +2,7 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 
-import products from './products'
 
 export default combineReducers({
   routing: routerReducer,
-  products,
 })
