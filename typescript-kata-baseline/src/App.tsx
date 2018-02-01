15acd93b4325ff92ccae71751872ac960b8e6eb8
MikielAgutu <mikiel.agutu@red-gate.com>
2018-02-01 09:17:50 +0000

    Remove unnecessary code and files

--- typescript-kata-baseline/src/App.tsx
+++ typescript-kata-baseline/src/App.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react';
 import './App.css';
 
-const logo = require('./logo.svg');
-
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Kata baseline for Typescript</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
