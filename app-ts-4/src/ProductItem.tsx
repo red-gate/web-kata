d0f8273524b4988cc381b00603877c2f66d18728
MikielAgutu <mikiel.agutu@red-gate.com>
2018-03-16 15:28:41 +0000

    Port ProductItem to Typescript

--- app-ts-4/src/ProductItem.tsx
+++ app-ts-4/src/ProductItem.tsx
@@ -1,7 +1,13 @@
-import React, { Component } from 'react'
+import * as React from 'react';
+import { Component } from 'react';
+import { Product } from './Models/Product'
 import './ProductItem.css'
 
-class ProductItem extends Component {
+interface ProductItemProps {
+  product: Product
+}
+
+class ProductItem extends Component<ProductItemProps, {}> {
   render() {
     const name = this.props.product.name
     return <div className='product-item'>
