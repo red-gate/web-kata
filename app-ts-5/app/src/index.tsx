78ca04272d0d7aaf3611fb4b4b555c12d0a3c4f0
santiaago <santiago.ariassar@gmail.com>
2018-04-25 09:41:37 +0100

    add browser router at root

--- app-ts-5/app/src/index.tsx
+++ app-ts-5/app/src/index.tsx
@@ -2,5 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
