fc29b2aa3fe43a4ab1a2df65d77a1f842c045d3a
santiaago <santiago.ariassar@gmail.com>
2018-06-29 14:42:26 +0100

    kata 6 - ts - use react-router-redux vnext

--- app-ts-6/app/src/index.tsx
+++ app-ts-6/app/src/index.tsx
@@ -2,16 +2,16 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 import { Provider } from 'react-redux';
 
-import store from './store';
+import store, { history } from './store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <ConnectedRouter history={history}>
       <App />
-    </BrowserRouter>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 );
--- app-ts-6/app/src/store.ts
+++ app-ts-6/app/src/store.ts
@@ -2,14 +2,18 @@ import { Store, createStore, applyMiddleware, compose } from 'redux';
 
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-
+import { routerMiddleware } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
 import rootReducer from './modules';
+
+export const history = createHistory();
 
 const initialState = {};
 
 const middleware = [
   thunk,
   logger,
+  routerMiddleware(history)
 ];
 
 const composedEnhancers = compose(
