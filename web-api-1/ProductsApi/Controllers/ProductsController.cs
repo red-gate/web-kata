fc3f20d2a790fb153dadc20e33039f82cee156b9
MikielAgutu <mikiel.agutu@red-gate.com>
2018-08-17 13:37:33 +0100

    Add ProductsController

--- web-api-1/ProductsApi/Controllers/ProductsController.cs
+++ web-api-1/ProductsApi/Controllers/ProductsController.cs
@@ -1,12 +1,3 @@
-1. `dotnet new webapi -n "ProductsApi"`
-1. `dotnet restore`
-1. `dotnet build`
-1. `dotnet run`
-1. Navigate to `http://localhost:5000/api/Values` to check it works
-
-Delete `ValuesController` and add new class `EventsController` with contents:
-
-``` C#
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,5 +16,3 @@ namespace ProductsApi.Controllers
         }
     }
 }
-
-```
