52bba2aad3673adf91048c20ce3ac2ba428574ac
Aaron Cooper <aaron.cooper@red-gate.com>
2018-09-21 15:36:13 +0100

    1

--- app-ts-1/src/App.tsx
+++ app-ts-1/src/App.tsx
@@ -1,22 +1,29 @@
 import * as React from 'react';
 import { Component } from 'react';
 import './App.css';
+import { GetData } from './data';
+
+const products = GetData();
+const productList = products.map((product) =>
+  <li>{product}</li>
+);
 
 class App extends Component {
-  render(): JSX.Element {
-    return (
-      <div className='App'>
-        <div className='App-header'>
-          <h2>Welcome Introduction to <code>web-kata#1-Typescript</code></h2>
-        </div>
-        <p className='App-intro'>
-          To get started change this text and then save to reload.
+
+render(): JSX.Element {
+  return (
+    <div className='App'>
+      <div className='App-header'>
+        <h2>Welcome Introduction to <code>web-kata#1-Typescript</code></h2>
+      </div>
+      <p className='App-intro'>
+        To get started change this text and then save to reload.
         </p>
-        <div className='products'>
-          ... your code here
-        </div>
-      </div>);
-  }
+      <div className='products'>
+        <ul>{ShowProducts()}</ul>
+      </div>
+    </div>);
+}
 }
 
 export default App;
