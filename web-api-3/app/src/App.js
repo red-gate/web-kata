6f8d17886df52c9af62296a99f5d548cc42bd652
MikielAgutu <mikiel.agutu@red-gate.com>
2018-10-05 15:32:14 +0100

    Kata 3 solution

--- web-api-3/app/src/App.js
+++ web-api-3/app/src/App.js
@@ -3,12 +3,19 @@ import { Route } from 'react-router-dom'
 
 import ProductMenu from './ProductMenu.js'
 import ProductContainer from './ProductContainer.js'
+import ProductForm from './ProductForm.js'
 import './App.css'
+
 
 class App extends Component {
 
   constructor(props) {
     super(props)
+    fetch('http://localhost:1786/api/products')
+      .then(response => response.json())
+      .then(products => this.setState({products}))
+      .catch(error => console.log(error));
+
     this.state = { products: [] }
   }
 
@@ -17,6 +24,7 @@ class App extends Component {
       <div className="App-header">
         <h2>Redgate products</h2>
       </div>
+      <ProductForm />
       <div className='products-container'>
         <ProductMenu products={this.state.products} />
         <Route exact path='/products/:productName' component={
