4de9ad32d47a86a8d22db0ef77533d8e59aceede
Thomas Walsh <thomas.walsh@red-gate.com>
2019-01-22 13:50:47 +0000

    Now retrieves products from in-memory storage

---
+++ app-website-1/ProductsApi/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ProductsApi.Model;
+
+namespace ProductsApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly List<Product> m_Products;
+
+        public ProductsController()
+        {
+            m_Products = new List<Product>();
+            m_Products.Add(new Product("SQL Source Control", "fdabndlfb"));
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return m_Products;
+        }
+    }
+}
---
+++ app-website-1/ProductsApi/Model/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsApi.Model
+{
+    public class Product
+    {
+        public string Name { get; }
+        public string Description { get; }
+
+        public Product(string name , string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
