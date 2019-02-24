3e9bd314258c4d3f575fe2bd63697e5670444454
David Halewood <davidnhalewood@hotmail.com>
2019-02-24 18:53:33 +0000

    Add Token Generator

---
+++ app-website-4/ProductsApi/Auth/TokenProvider.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ProductsApi.Auth
+{
+    public class TokenProvider
+    {
+        public string GenerateToken(string[] usernameAndPassword)
+        {
+            if (usernameAndPassword[0] == "admin" && usernameAndPassword[1] == "password")
+            {
+                var claimsdata = new[] { new Claim(ClaimTypes.Name, usernameAndPassword[0]) };
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("This is a secret key"));
+                var signInCred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+                var token = new JwtSecurityToken(
+                     issuer: "Products API",
+                     audience: "Products App",
+                     expires: DateTime.Now.AddMinutes(30),
+                     claims: claimsdata,
+                     signingCredentials: signInCred
+                    );
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                return tokenString;
+            }
+
+            return null;
+        }
+    }
+}
